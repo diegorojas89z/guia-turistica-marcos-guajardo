a1c33e5c9a0dc49e0911145527c2e9a6d13ef3bf
Diego Rojas Brañez <107219408+diegorojas89z@users.noreply.github.com>
2025-11-21 14:47:53 -0300

    1er commit de trabajo de Marcos Guajardo
    se usará este repositorio como ejemplo para muestra de deploy

---
+++ newFile.js
@@ -0,0 +1,8 @@
+document.querySelector('.menu-icon').addEventListener('click', () => {
+});
+document.querySelector('.search-icon').addEventListener('click', () => {
+});
+document.querySelectorAll('.btn').forEach(btn => {
+  btn.addEventListener('click', () => {;
+  });
+});
